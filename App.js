b8bb95a5051ad07cefc7cb4119be26b927200040
George Kibe <georgekibewambui@gmail.com>
2022-05-10 16:24:35 +0300

    Testing importing vector icons

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Entype from "react-native-vector-icons"
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app! Uberclone here we go!</Text>
+      <Text>AirBnb here we go!</Text>
+      <Text>AirBnb here we go!</Text>
+      {/* <Entype name={"Analytics"} /> */}
       <StatusBar style="auto" />
     </View>
   );
