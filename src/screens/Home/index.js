8abe31c6f1be718b79729094c8bc89ae33494efd
George Kibe <georgekibewambui@gmail.com>
2022-05-11 15:20:12 +0300

    working on the homescreen

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,16 +1,27 @@
-import { View, Text, ImageBackground } from 'react-native'
+import { View, Text, ImageBackground, Pressable } from 'react-native'
 import React from 'react'
 import styles from './styles'
-
+import Fontisto from "react-native-vector-icons/Fontisto"
 
 const HomeScreen = () => {
+    const color = "#f15454"
   return (
-    <View>
+    <View >
       <ImageBackground source={require("../../../assets/images/wallpaper.jpg")}
                         style={styles.image}>
+        <Pressable style={styles.searchButton}
+            onPress={() => console.warn("Search Btn Clicked")}>
+            <Fontisto name="search" size={25} color={color} />
+            <Text style={styles.searchButtonText}>Where are you going?</Text>
+        </Pressable>
+
         <Text style={styles.title}>Get Close Properties</Text>
+        <Pressable style={styles.button}
+            onPress={() => console.warn("Explore Btn Clicked")}>
+            <Text style={styles.buttonText}>Explore Nearby Properties</Text>
+        </Pressable>
       </ImageBackground>  
-      <Text>HomeScreenssssssssss</Text>
+      
     </View>
   )
 }
--- src/screens/Home/styles.js
+++ src/screens/Home/styles.js
@@ -1,9 +1,9 @@
-import { StyleSheet } from "react-native";
+import { StyleSheet, Dimensions } from "react-native";
 
 const styles = StyleSheet.create({
     image : {
         width:'100%',
-        height:500,
+        height:"90%",
         resizeMode:'cover',
         justifyContent: 'center'
     },
@@ -11,7 +11,40 @@ const styles = StyleSheet.create({
         fontSize:60,
         fontWeight:'bold',
         color:'white',
-        width:'60%'
+        width:'60%',
+        marginLeft:"5%"
+    },
+    button:{
+        backgroundColor:"#fff",
+        width:200,
+        height:40,
+        borderRadius:15,
+        marginLeft:"5%",
+        marginTop:"5%",
+        justifyContent:'center',
+        alignItems:'center'
+
+    },
+    buttonText:{
+        fontSize:16,
+        fontWeight:'bold',
+    },
+    searchButton:{
+        backgroundColor:"#fff",
+        height:60,
+        width:Dimensions.get('screen').width - 20,
+        borderRadius:30,
+        marginHorizontal:10,
+        flexDirection:'row',
+        justifyContent:'center',
+        alignItems:'center',
+        position:"absolute",
+        top:10,
+        zIndex:100,        
+    },
+    searchButtonText:{
+        fontSize:16,
+        fontWeight:'bold',
     }
 })
 
