c8c18cf17680723bec0278e2e35c7093f8a67354
George Kibe <georgekibewambui@gmail.com>
2022-05-16 15:16:52 +0300

    testing maps

--- src/navigation/HomeTabNavigator.js
+++ src/navigation/HomeTabNavigator.js
@@ -8,8 +8,10 @@ import FontAwesome5 from 'react-native-vector-icons/FontAwesome5'
 import Feather from 'react-native-vector-icons/Feather'
 import EvilIcons from 'react-native-vector-icons/EvilIcons'
 import ExploreNavigator from "./ExploreNavigator"
+import SearchResultsMap from "../screens/SearchResultsMap";
 
 const Tab = createBottomTabNavigator()
+
 
 const HomeTabNavigator = () => {
   return (
--- src/screens/SearchResultsMap/index.js
+++ src/screens/SearchResultsMap/index.js
@@ -1,6 +1,6 @@
 import { View, Text } from 'react-native'
 import React from 'react'
-import MapView from 'react-native-maps';
+import MapView, {Marker} from 'react-native-maps';
 
 
 const SearchResultsMap = () => {
@@ -8,13 +8,14 @@ const SearchResultsMap = () => {
     <View style={{width:"100%", height:"100%"}}>
       <MapView style={{width:"100%", height:"100%"}}
         initialRegion={{
-        latitude: 26.278825,
-        longitude: -16.5324,
+        latitude: 28.32,
+        longitude: -16.532,
         latitudeDelta: 0.8,
         longitudeDelta: 0.9,
-
         }}
-    />
+    >
+        
+    </MapView>
     </View>
   )
 }
