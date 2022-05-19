21998a47168830af969895c488f91913868ad852
George Kibe <georgekibewambui@gmail.com>
2022-05-19 19:14:14 +0300

    using useref to cater for property flatlist

--- src/screens/SearchResultsMap/index.js
+++ src/screens/SearchResultsMap/index.js
@@ -1,6 +1,6 @@
 import { View, FlatList, useWindowDimensions } from 'react-native'
-import React, {useState} from 'react'
-import MapView, {Marker, PROVIDER_GOOGLE} from 'react-native-maps';
+import React, {useState, useEffect, useRef} from 'react'
+import MapView, {PROVIDER_GOOGLE} from 'react-native-maps';
 import properties from "../../../assets/data/feed"
 import CustomMarker from '../../components/CustomMarker';
 import PropertyCarouselItem from '../../components/PropertyCarouselItem';
@@ -8,6 +8,34 @@ import PropertyCarouselItem from '../../components/PropertyCarouselItem';
 const SearchResultsMap = () => {
     const [selectedPropertyId, setSelectedPropertyId] = useState(null)
     const { height, width } = useWindowDimensions();
+    const flatList = useRef()
+    const map = useRef()
+
+    const viewConfig = useRef({itemVisiblePercentThreshold: 70})
+    const onViewChanged = useRef(({viewableItems}) => {
+      if (viewableItems.length > 0) {
+        const selectedProperty = viewableItems[0].item;
+        setSelectedPropertyId(selectedProperty.id)
+      }
+    })
+
+    useEffect(() => {
+      if(!selectedPropertyId || !flatList){
+        return
+      }
+      const index = properties.findIndex(property => property.id === selectedPropertyId)
+      flatList.current.scrollToIndex({index})
+
+      // const selectedProperty = properties[index];
+      // const region = {
+      //   latitude: selectedProperty.latitude,
+      //   longitude: selectedProperty.longitude,
+      //   latitudeDelta: 0.8,
+      //   longitudeDelta: 0.8,
+      // }
+      // map.current.animateToRegion(region);
+    }, [selectedPropertyId])
+    
     return (
     <View style={{width:"100%", height:"100%"}}>
       <MapView style={{width:"100%", height:"100%"}}
@@ -17,6 +45,7 @@ const SearchResultsMap = () => {
         latitudeDelta: 0.8,
         longitudeDelta: 0.8,
         }}
+        ref={map}
         provider={PROVIDER_GOOGLE}
         >
             {properties.map(property => (<CustomMarker property={property}
@@ -25,6 +54,7 @@ const SearchResultsMap = () => {
         </MapView>
         <View style={{position:"absolute", bottom:55, left:0}}>
               <FlatList
+                ref={flatList}
                 data={properties}
                 renderItem={({item}) => <PropertyCarouselItem property={item} />}
                 horizontal
@@ -32,6 +62,8 @@ const SearchResultsMap = () => {
                 snapToInterval={width-60}
                 snapToAlignment={"center"}
                 decelerationRate={"fast"}
+                viewabilityConfig={viewConfig.current}
+                onViewableItemsChanged={onViewChanged.current}
               />
             
         </View>
