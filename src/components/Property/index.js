ae90b4d357a60d7a4b6a3b7e11316164f41c042a
George Kibe <georgekibewambui@gmail.com>
2022-05-20 14:07:27 +0300

    handlind property navigation

--- src/components/Property/index.js
+++ src/components/Property/index.js
@@ -1,11 +1,17 @@
-import { View, Text, Image } from 'react-native'
+import { View, Text, Image, Pressable } from 'react-native'
 import React from 'react'
 import styles from './styles'
-
+import { useNavigation } from '@react-navigation/native';
 
 const Property = ({property}) => {
+  console.warn(property.id)
+  const navigation = useNavigation()
+  const goToProperty = () =>{
+    navigation.navigate("Detailed Property", {propertyId:property.id});
+  }
+
   return (
-    <View style={styles.container}>
+    <Pressable onPress={goToProperty} style={styles.container}>
       <Image style={styles.image}
         source={{uri:property.image}}/>
       <Text style={styles.bedrooms}>{property.bedroom}-Bedroom {property.title}</Text>
@@ -18,7 +24,7 @@ const Property = ({property}) => {
            Per Year
       </Text>
       <Text style={styles.totalPrice}>${property.totalPrice} Total</Text>
-    </View>
+    </Pressable>
   )
 };
 
--- src/screens/PropertyScreen/index.js
+++ src/screens/PropertyScreen/index.js
@@ -2,9 +2,14 @@ import { View, Text } from 'react-native'
 import React from 'react'
 import DetailedProperty from "../../components/DetailedProperty"
 import properties from "../../../assets/data/feed"
+import { useRoute } from '@react-navigation/native'
 
-const property = properties[2]
+
+//const property = properties[2]
 const PropertyScreen = () => {
+  const route = useRoute()
+  console.warn(route)
+  const property = properties.find(property => property.id === route.params.propertyId)
   return (
     <View style={{backgroundColor:"white"}}>
       <DetailedProperty property={property} />
