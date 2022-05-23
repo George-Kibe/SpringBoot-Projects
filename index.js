a9a6d8fc404665cc8258f8ac10b6c33802a3b93b
George Kibe <georgekibewambui@gmail.com>
2022-05-23 23:10:41 +0300

    configuring aws-amplify

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 import { registerRootComponent } from 'expo';
 
+//import {AppRegistry} from "react-native"
+import {name as AppName} from "./app.json"
+
+import { Amplify } from 'aws-amplify'
+import awsconfig from './src/aws-exports'
+Amplify.configure(awsconfig)
+
 import App from './App';
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
 // the environment is set up appropriately
 registerRootComponent(App);
+
+//AppRegistry.registerComponent(appName, getComponenctFunc: () => App)
