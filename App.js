975d4025c13c3a196700ee3ebfcf320055221413
George-Kibe <georgekibewambui@gmail.com>
2023-07-05 15:07:12 +0300

    installing react navigation

--- App.js
+++ App.js
@@ -1,45 +1,20 @@
 import React from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
-import {Colors} from 'react-native/Libraries/NewAppScreen';
-import FontAwesome from 'react-native-vector-icons/FontAwesome';
+import MainNavigation from './src/navigation';
+import {SafeAreaView, StyleSheet} from 'react-native';
+
 function App() {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <View
-        contentInsetAdjustmentBehavior="automatic"
-        style={styles.container}
-      />
-      <Text>Hello</Text>
-      <FontAwesome name="phone" />
+    <SafeAreaView style={styles.container}>
+      <MainNavigation />
     </SafeAreaView>
   );
 }
 
+export default App;
+
 const styles = StyleSheet.create({
   container: {
-    marginTop: 32,
-    paddingHorizontal: 24,
     display: 'flex',
     flex: 1,
   },
 });
-
-export default App;
