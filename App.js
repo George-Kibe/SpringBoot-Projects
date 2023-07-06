0e48c7fe7b8d2ae417d182f66e55d0e9c7942ede
George-Kibe <georgekibewambui@gmail.com>
2023-07-06 14:59:37 +0300

    new design

--- App.js
+++ App.js
@@ -1,20 +1,38 @@
-import React from 'react';
-import MainNavigation from './src/navigation';
-import {SafeAreaView, StyleSheet} from 'react-native';
+/* eslint-disable react/react-in-jsx-scope */
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {SafeAreaProvider} from 'react-native-safe-area-context';
+import {
+  Walkthrough,
+  Verification,
+  ProfileAccount,
+  PhoneNumber,
+  PersonalChat,
+} from './src/screens';
+import BottomTabNavigation from './src/navigation/BottomTabNavigation';
 
-function App() {
+const Stack = createNativeStackNavigator();
+
+export default function App() {
   return (
-    <SafeAreaView style={styles.container}>
-      <MainNavigation />
-    </SafeAreaView>
+    <SafeAreaProvider>
+      <NavigationContainer>
+        <Stack.Navigator
+          screenOptions={{
+            headerShown: false,
+          }}
+          initialRouteName="Walkthrough">
+          <Stack.Screen
+            name="BottomTabNavigation"
+            component={BottomTabNavigation}
+          />
+          <Stack.Screen name="Walkthrough" component={Walkthrough} />
+          <Stack.Screen name="Verification" component={Verification} />
+          <Stack.Screen name="ProfileAccount" component={ProfileAccount} />
+          <Stack.Screen name="PhoneNumber" component={PhoneNumber} />
+          <Stack.Screen name="PersonalChat" component={PersonalChat} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </SafeAreaProvider>
   );
 }
-
-export default App;
-
-const styles = StyleSheet.create({
-  container: {
-    display: 'flex',
-    flex: 1,
-  },
-});
---
+++ src/components/PageTitle.js
@@ -0,0 +1,40 @@
+/* eslint-disable react-native/no-inline-styles */
+import {View, Text, StyleSheet} from 'react-native';
+import React from 'react';
+import {TouchableOpacity} from 'react-native';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
+import {SIZES, COLORS, FONTS} from '../constants';
+
+const PageTitle = props => {
+  return (
+    <View style={styles.pageTitleContainer}>
+      <TouchableOpacity
+        onPress={props.onPress}
+        style={{
+          marginRight: 12,
+        }}>
+        <MaterialIcons
+          name="keyboard-arrow-left"
+          size={SIZES.padding * 3}
+          color={COLORS.black}
+        />
+      </TouchableOpacity>
+      {props.title && (
+        <Text style={{...FONTS.h4, color: COLORS.black}}>
+          {props?.title || 'No Page Title'}
+        </Text>
+      )}
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  pageTitleContainer: {
+    marginHorizontal: 22,
+    marginVertical: 22,
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+});
+
+export default PageTitle;
