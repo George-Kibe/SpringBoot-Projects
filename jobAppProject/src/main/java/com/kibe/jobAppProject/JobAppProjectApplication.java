48a8cbfd034e2e6ea2d8b40b50de3f0daba0699f
George-Kibe <georgekibewambui@gmail.com>
2024-03-12 21:26:55 +0300

    docker compose and testing

--- jobAppProject/src/main/java/com/kibe/jobAppProject/JobAppProjectApplication.java
+++ jobAppProject/src/main/java/com/kibe/jobAppProject/JobAppProjectApplication.java
@@ -2,7 +2,7 @@ package com.kibe.jobAppProject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+// continue from 8:54:40
 @SpringBootApplication
 public class JobAppProjectApplication {
 
